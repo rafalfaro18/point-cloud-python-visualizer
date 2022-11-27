2a860dac2626bd561ef273aa94761b6602584f99
Rafael Alfaro <rafalfaro_18@outlook.com>
2022-11-26 18:40:23 -0600

    opens voxel version after closing point cloud

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import open3d as o3d
 pcd = o3d.io.read_point_cloud('sfm.ply')
 o3d.visualization.draw_geometries([pcd])
 
-#voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=0.004)
-#o3d.visualization.draw_geometries([voxel_grid])
+voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=0.004)
+o3d.visualization.draw_geometries([voxel_grid])
